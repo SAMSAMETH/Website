3265aea4112ec807d59eee484a173cf0766c6bda
SAMSAMETH <18samuelstephen@gmail.com>
2025-11-13 21:38:28 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
   ],
+
+  server: {
+    allowedHosts: [
+      "unslanted-fay-introductory.ngrok-free.dev"
+    ],
+    host: true,
+    port: 5173
+  }
 })
